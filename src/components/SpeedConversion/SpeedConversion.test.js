0d339206f34fa410374d170c011a92b1d01c8d2d
Sergiu Buciuc <sbuciuc@axway.com>
2017-02-16 18:12:22 +0200

    added test

--- src/components/SpeedConversion/SpeedConversion.test.js
+++ src/components/SpeedConversion/SpeedConversion.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import SpeedConversion from './SpeedConversion';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 it('renders with default value 10 kph', () => {
   const wrapper = shallow(<SpeedConversion />);
   expect(wrapper.find('input[value=10]')).toHaveLength(1);
+});
+
+it('fires onChange in response to input change', () => {
+  const onChange = sinon.spy();
+  const wrapper = shallow(<SpeedConversion onChange={onChange}/>);
+  const input = wrapper.find('input[value=10]');
+  input.simulate('change', { target: { value: 1 }} );
+  expect(onChange.calledWith(1)).toBe(true);
 });
