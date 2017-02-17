e3969760ef9e18e67a5b64724374449e1a8876b2
Sergiu Buciuc <sbuciuc@axway.com>
2017-02-17 09:42:45 +0200

    convert to class

--- src/components/SpeedConversion/SpeedConversion.js
+++ src/components/SpeedConversion/SpeedConversion.js
@@ -8,29 +8,32 @@ import {
   minMileToMinKm,
 } from '../../services/conversion';
 
-export default function SpeedConversion({ kph, onChange}) {
-  return (
-    <div>
-      <input
-        className="kph"
-        type="number"
-        value={kph}
-        onChange={(event) => onChange(event.target.value)}
-      />
-      <input
-        className="minKm"
-        type="text"
-        value={minToTime(kphToMinKm(kph))}
-        onChange={(event) => onChange(minKmToKph(timeToMin(event.target.value)))}
-      />
-      <input
-        className="minMile"
-        type="text"
-        value={minToTime(minKmToMinMile(kphToMinKm(kph)))}
-        onChange={(event) => onChange(minKmToKph(minMileToMinKm(timeToMin(event.target.value))))}
-      />
-    </div>
-  );
+export default class SpeedConversion extends React.Component {
+  render() {
+    const { kph, onChange} = this.props;
+    return (
+      <div>
+        <input
+          className="kph"
+          type="number"
+          value={kph}
+          onChange={(event) => onChange(event.target.value)}
+        />
+        <input
+          className="minKm"
+          type="text"
+          value={minToTime(kphToMinKm(kph))}
+          onChange={(event) => onChange(minKmToKph(timeToMin(event.target.value)))}
+        />
+        <input
+          className="minMile"
+          type="text"
+          value={minToTime(minKmToMinMile(kphToMinKm(kph)))}
+          onChange={(event) => onChange(minKmToKph(minMileToMinKm(timeToMin(event.target.value))))}
+        />
+      </div>
+    );
+  }
 }
 
 SpeedConversion.propTypes = {
