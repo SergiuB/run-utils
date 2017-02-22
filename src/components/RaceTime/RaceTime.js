dc64f71541174265cd0b42d8f41bae5d34f04563
Sergiu Buciuc <sbuciuc@axway.com>
2017-02-22 10:18:32 +0200

    RaceTime onChange

--- src/components/RaceTime/RaceTime.js
+++ src/components/RaceTime/RaceTime.js
@@ -3,6 +3,8 @@ import TimePicker from 'rc-time-picker';
 import moment from 'moment';
 import {
   secToTime,
+  minKmToKph,
+  timeToMin,
 } from '../../services/conversion';
 
 import { raceTime } from '../../services/raceCalculator';
@@ -11,10 +13,11 @@ import 'rc-time-picker/assets/index.css';
 
 export default class RaceTime extends React.Component {
   render() {
-    const { kph, km} = this.props;
+    const { kph, km, onChange} = this.props;
     return (
       <TimePicker
         value={moment(secToTime(raceTime(kph, km)), 'HH:mm:ss')}
+        onChange={(val) => onChange(minKmToKph(timeToMin(val.format('mm:ss'))))}
       />
     );
   }
@@ -23,4 +26,9 @@ export default class RaceTime extends React.Component {
 RaceTime.propTypes = {
   kph: React.PropTypes.number.isRequired,
   km: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func,
 };
+
+RaceTime.defaultProps = {
+  onChange: () => {},
+}
--- src/components/RaceTime/RaceTime.test.js
+++ src/components/RaceTime/RaceTime.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import TimePicker from 'rc-time-picker';
+import moment from 'moment';
+import sinon from 'sinon';
 
 import RaceTime from './RaceTime';
 
 it('renders', () => {
   const wrapper = shallow(<RaceTime kph={10} km={1}/>);
   expect(wrapper.find(TimePicker).props().value.format('HH:mm:ss')).toEqual('00:06:00');
+});
+
+it('fires onChange in response to input change', () => {
+  const onChange = sinon.spy();
+  const wrapper = shallow(<RaceTime km={1} kph={10} onChange={onChange}/>);
+  const input = wrapper.find(TimePicker);
+  input.simulate('change', moment('00:10:00', 'HH:mm:ss') );
+  expect(onChange.calledWith(6)).toBe(true);
 });
