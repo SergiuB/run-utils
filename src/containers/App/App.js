93e01b6f424983ed33ead4eaa43083d4c26c0aae
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2017-04-14 13:39:11 +0300

    VdotPerformance component in app.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,98 +2,26 @@ import React, { Component } from 'react';
 import logo from '../../assets/logo.svg';
 import './App.css';
 
-import SpeedConversion from '../../components/SpeedConversion';
-import RaceTime from '../../components/RaceTime';
-import RaceSlider from '../../components/RaceSlider';
+import VdotPerformance from '../../containers/VdotPerformance';
 import {
-  allRaces,
   kMarathon,
   kHalf,
   k10,
   k5,
+  k3,
   kMile,
-  k1500,
 } from '../../services/constants';
 
-const MIN_KPH = 7;
-const MAX_KPH = 36;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { kph: 11 };
-  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SpeedConversion
-          kph={this.state.kph}
-          onChange={kph => this.setState({ kph })}
+        <VdotPerformance
+            baseRaceTime={'1:36:33'}
+            baseRace={kHalf}
+            races={[kMarathon, kHalf, k10, k5, k3, kMile]}
         />
-        <div>
-          {allRaces.map(({ label, distance }) => (
-            <div key={label}>
-              <label>{label}</label>
-              <RaceTime
-                showHour={distance > k5.distance}
-                km={distance}
-                kph={this.state.kph}
-                onChange={kph => this.setState({ kph })}
-              />
-            </div>
-          ))}
-        </div>
-        <div className="App-sliders">
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={k1500}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={kMile}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={k5}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={k10}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={kHalf}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-          <RaceSlider
-            kph={this.state.kph}
-            onChange={kph => this.setState({ kph })}
-            race={kMarathon}
-            minKph={MIN_KPH}
-            maxKph={MAX_KPH}
-          />
-      </div>
       </div>
     );
   }
