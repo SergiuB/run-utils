a9543a65beb1d5ebced97336c91ca7d8819c9791
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2017-05-10 11:41:49 +0300

    workout parser using pegjs

---
+++ src/services/workoutGrammar.pegjs
@@ -0,0 +1,32 @@
+{ 
+    function leftAssoc(rest, val) {
+        if (!rest.length) return val;
+        var last = rest.pop();
+        var left = leftAssoc(rest, last[0]);
+        return left.length? [...left, val] : [left, val];
+    }
+}
+
+Start = E1
+
+E1 = rest:(E2 "+")* v:E2 { return leftAssoc(rest, v); }
+
+E2 = times:Number '*' work:WorkoutPart { return {times, work} }
+	/ WorkoutPart
+            
+WorkoutPart = Segment
+			/ BracketedExpression
+            
+BracketedExpression = "(" seq:E1 ")" { return seq; }
+
+Segment = work:BasicSegment type:SegmentType "/" rest:BasicSegment { return {work, type, rest}; }
+		/ work:BasicSegment type:SegmentType { return {work, type}; }
+
+BasicSegment = num:Number unit:(TimeUnit / LengthUnit) { return {num, unit}; }
+
+SegmentType = "E" / "M" / "T" / "I" / "R"
+
+Number = [1-9][0-9]* { return parseInt(text(), 10); }
+
+TimeUnit = "\'" / "\""
+LengthUnit = "km" / "mi" / "m"
---
+++ src/services/workoutParser.test.js
@@ -0,0 +1,105 @@
+import R from 'ramda';
+import { parse } from './workoutParser';
+
+it('parses basic workouts', () => {
+  expect(parse('2kmE')).toEqual({
+    'type': 'E',
+    'work': {
+      'num': 2,
+      'unit': 'km',
+    }
+  });
+
+  expect(parse('20\'T')).toEqual({
+    'type': 'T',
+    'work': {
+      'num': 20,
+      'unit': '\'',
+    }
+  });  
+});
+
+it('parses interval workouts', () => {
+  expect(parse("1200mT/3'")).toEqual({
+    "work": {
+        "num": 1200,
+        "unit": "m"
+    },
+    "type": "T",
+    "rest": {
+        "num": 3,
+        "unit": "'"
+    }
+  });
+
+  expect(parse('400mR/400m')).toEqual({
+   "work": {
+      "num": 400,
+      "unit": "m"
+   },
+   "type": "R",
+   "rest": {
+      "num": 400,
+      "unit": "m"
+   }
+});  
+});
+
+it('parses workout sequences', () => {
+  expect(parse("3200mE+2*(3*400mR/400m+5kmT+3*3'I/20\")+3200mE")).toEqual([
+    {
+        "work": {
+          "num": 3200,
+          "unit": "m"
+        },
+        "type": "E"
+    },
+    {
+        "times": 2,
+        "work": [
+          {
+              "times": 3,
+              "work": {
+                "work": {
+                    "num": 400,
+                    "unit": "m"
+                },
+                "type": "R",
+                "rest": {
+                    "num": 400,
+                    "unit": "m"
+                }
+              }
+          },
+          {
+              "work": {
+                "num": 5,
+                "unit": "km"
+              },
+              "type": "T"
+          },
+          {
+              "times": 3,
+              "work": {
+                "work": {
+                    "num": 3,
+                    "unit": "'"
+                },
+                "type": "I",
+                "rest": {
+                    "num": 20,
+                    "unit": "\""
+                }
+              }
+          }
+        ]
+    },
+    {
+        "work": {
+          "num": 3200,
+          "unit": "m"
+        },
+        "type": "E"
+    }
+  ]);
+});
