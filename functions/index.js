5fe74608ec916aae4b40479cbc88512e804865f4
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2017-05-17 16:46:50 +0300

    strava auth with firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -15,22 +15,9 @@ const oauthIds = {
   strava: {
     clientID: '9244',
     clientSecret: '94b8617916ead111be4ebf700ca1d610e2fb2ff0',
-    callbackURL: 'https://us-central1-run-utils.cloudfunctions.net/stravaCallback'
+    callbackURL: 'https://us-central1-run-utils.cloudfunctions.net/api/stravaCallback'
   }
 };
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push it into the Realtime Database then send a response
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
-});
-
 
 router.use(cors);
 router.use(session({
@@ -67,44 +54,35 @@ passport.use(new StravaStrategy({
   }
 ));
 
-router.get('/hello', ({ user, url }, res) => {
-  // const user = req.user;
-  // console.log('user is', user);
-  // console.log(req.originalUrl); // '/admin/new'
-  // console.log(req.baseUrl); // '/admin'
-  // console.log(req.path); // '/new'
-  console.log('url is', url); // '/new'
+const enableIf = (predicateFn, mwToEnable) => (req, res, next) => {
+  const enable = predicateFn(req);
+  enable && mwToEnable(req, res, next);
+  !enable && next();
+}
+
+const authenticateIfNot = enableIf(req => !req.user, passport.authenticate('strava', { failureRedirect: 'error' }));
+const log = ({ user, url }, req, next) => {
+  console.log(`user: ${user}, url: ${url}`);
+  next();
+}
+
+router.use(log);
+
+router.get('/hello',
+  authenticateIfNot,
+  (req, res) => {
+  const { user } = req;
   res.send(user ? 'Hello user ' + user : 'Dont know you!' );
 });
 
 router.get('/stravaCallback',
-  (req, res, next) => { console.log('stravaCallback found'); next(); },
-  (req, res) => res.send('stravacallback ok')
-  // passport.authenticate('strava', { successRedirect: '/',
-  //                                  failureRedirect: '/?error=true',
-  //                                  failureFlash: true })
+  passport.authenticate('strava', { successRedirect: 'hello',
+                                   failureRedirect: 'error',
+                                   failureFlash: true })
 );
 
 router.get('/authStrava',
-   (req, res, next) => { console.log('authStrava found'); next(); },
    passport.authenticate('strava')
 );
 
-const httpsFunction = funcName => functions.https.onRequest((req, res) => {
-  console.log('new request ', funcName);
-  // NOTE: Adding a temporary fix for issue https://github.com/firebase/firebase-functions/issues/27
-  // const splitUrl = req.url.split('?');
-  // const queryParams = splitUrl[splitUrl.length - 1];
-  // console.log(req.url, splitUrl, queryParams);
-  // if (queryParams)
-  //   req.url = `/${funcName}?${queryParams}`;
-  // else
-    req.url = `/${funcName}`;
-    console.log('query: ', req.query);
-  console.log('url is ', req.url);
-  return router(req, res);
-});
-
-exports.hello = httpsFunction('hello');
-exports.stravaCallback = httpsFunction('stravaCallback');
-exports.authStrava = httpsFunction('authStrava');
+exports.api = functions.https.onRequest(router);
